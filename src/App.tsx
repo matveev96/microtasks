c0b74e2e65b1222c975ca00bb84b21ff319654b7
matveev96 <sasha.matveev1996@icloud.com>
2024-11-07 23:50:58 +0300

    start microtask with Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
-import {data1, data2, Tasks} from "./components/Tasks";
-import {TableCars, topCars} from "./components/TableCars";
+// import {data1, data2, Tasks} from "./components/Tasks";
+// import {TableCars, topCars} from "./components/TableCars";
+import Button from "./components/Button";
 
-
-// Hi Guys!
-// Let's reinforce our current session!
-// -You have 2 arrays. You should create a new component TASKS, where you will render these arrays.
-// -Don't forget to assign types to our data.
 
 
 function App() {
+    const Button1Foo = (subscriber: string, age:number, address: string) => {
+        return console.log(subscriber, age, address)
+    }
+    const Button2Foo = (subscriber: string, age:number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+    const ButtonStupid = () => {
+        console.log('stupid button')
+    }
 
     return (
         <div className="App">
-            <Tasks data={data1} />
-            <Tasks data={data2}/>
-            <TableCars data={topCars}/>
+            {/*<Tasks data={data1} />*/}
+            {/*<Tasks data={data2}/>*/}
+            {/*<TableCars data={topCars}/>*/}
+            <Button name={'MyYoutubeChanel-1'} callBack={()=>Button1Foo('im ilya', 21, 'Unosti')}/>
+            <Button name={'MyYoutubeChanel-2'} callBack={()=>Button2Foo('im vanya', 23, 'Mira')}/>
+            <Button name={'ButtonStupid'} callBack={ButtonStupid}/>
         </div>
     );
 }
 
 export default App;
-
