7df8cdef7ce959b014d95f3d711d6a7353fb3883
matveev96 <sasha.matveev1996@icloud.com>
2024-11-08 23:42:05 +0300

    start microtask with State

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 // import {data1, data2, Tasks} from "./components/Tasks";
 // import {TableCars, topCars} from "./components/TableCars";
 import Button from "./components/Button";
+import State from "./components/State";
 
 
 
@@ -25,6 +26,7 @@ function App() {
             <Button name={'MyYoutubeChanel-1'} callBack={()=>Button1Foo('im ilya', 21, 'Unosti')}/>
             <Button name={'MyYoutubeChanel-2'} callBack={()=>Button2Foo('im vanya', 23, 'Mira')}/>
             <Button name={'ButtonStupid'} callBack={ButtonStupid}/>
+            <State/>
         </div>
     );
 }
